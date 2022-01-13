d77e09e06bc3c84d8bd85f35b80b164b94c9ba41
josephbharrison <22684038+josephbharrison@users.noreply.github.com>
2022-01-13 12:55:48 -0500

    feat(api): conditionally remove alignment gaps feat(api): allow sequences query to include full fasta (description and sequence, and with newlines) fix(genbank): remove temporary files (xml, json, and fas query)

--- api/sequence.py
+++ api/sequence.py
@@ -1,3 +1,4 @@
+import os
 import hashlib
 import json
 import xmltodict
@@ -7,7 +8,7 @@ from subprocess import Popen, PIPE
 
 def log_results(results: list):
     """
-    Log results
+    Log command results
     :param results:
     :return:
     """
@@ -24,8 +25,7 @@ def log_results(results: list):
 
 def execute(commands: list) -> list:
     """
-    Function that runs a series of command string,
-    raises exception on failure
+    Function that executes a series of command strings, and raises exception on failure
     :param commands: list of command strings
     """
     results = []
@@ -40,11 +40,21 @@ def execute(commands: list) -> list:
 
 
 def get_id(body: dict) -> str:
+    """
+    Generate Id from hash of body
+    :param body:
+    :return:
+    """
     body_str = json.dumps(body)
     return hashlib.md5(body_str.encode('utf-8')).hexdigest()
 
 
 def load_xml(file: str) -> dict:
+    """
+    Return dictionary from XML file
+    :param file:
+    :return:
+    """
 
     with open(file) as f:
         data = xmltodict.parse(f.read())
@@ -54,6 +64,11 @@ def load_xml(file: str) -> dict:
 
 
 def load_json(file: str) -> dict:
+    """
+    Return dictionary from JSON file
+    :param file:
+    :return:
+    """
 
     with open(file) as f:
         data = json.load(f)
@@ -63,18 +78,36 @@ def load_json(file: str) -> dict:
 
 
 def write_json(data: dict, file: str):
+    """
+    Write JSON file from dict
+    :param data:
+    :param file:
+    :return:
+    """
 
     with open(file, 'w') as f:
         json.dump(data, f)
 
 
 def write_file(file: str, lines: list):
+    """
+    Write file from list of lines
+    :param file:
+    :param lines:
+    :return:
+    """
+
     f = open(file, "w")  # append mode
     f.writelines(lines)
     f.close()
 
 
 def load_accessions(blast_results: dict) -> list:
+    """
+    Return list of accession dictionaries from blastn results
+    :param blast_results:
+    :return:
+    """
     report = blast_results['BlastOutput2'][0]['report']
     hits = report['results']['search']['hits']
 
@@ -91,7 +124,7 @@ def load_accessions(blast_results: dict) -> list:
         description = hit['description'][0]['title']
         hit_sequence = hit['hsps'][0]['hseq']
         accession = {'id': accession_id, 'description': description, 'sequence': hit_sequence}
-        print(json.dumps(accession))
+        logging.debug(json.dumps(accession))
         accessions.append(accession)
 
     return accessions
@@ -103,7 +136,49 @@ def fetch_accession(xml_file: str, accession_id: str):
     log_results(command_results)
 
 
-def load_fastas(id: str, accessions: list, add_location: bool = True) -> list:
+def clean_fasta(fasta_sequence: str, remove_gaps: bool = True) -> dict:
+    """
+    Clean FASTA sequence
+    :param fasta_sequence:
+    :param remove_gaps:
+    :return:
+    """
+
+    lines = fasta_sequence.splitlines()
+    description = ''
+    seq = []
+    for line in lines:
+        if '>' in line:
+            description = line
+        else:
+            seq.append(line)
+
+    sequence = ''.join(seq)
+
+    gap_chars = ['-', '.']
+
+    if remove_gaps:
+        for gap_char in gap_chars:
+            logging.debug(f'removing gap char: "{gap_char}", from: {sequence}')
+            print(f'removing gap char: "{gap_char}", from: {sequence}')
+            sequence = sequence.replace(gap_char, '')
+            logging.debug(f'clean_sequence: {sequence}')
+            print(f'clean_sequence: {sequence}')
+
+    fasta = {'description': description, 'sequence': sequence}
+
+    return fasta
+
+
+def load_fasta(id: str, accessions: list, add_location: bool = True, remove_gaps: bool = True) -> list:
+    """
+    Return list of fasta dictionaries from accessions
+    :param id:
+    :param accessions:
+    :param add_location:
+    :param remove_gaps:
+    :return:
+    """
 
     fastas = []
     # run efetch to pull all accession details
@@ -116,26 +191,47 @@ def load_fastas(id: str, accessions: list, add_location: bool = True) -> list:
         a_object = load_xml(xml_file)
         write_json(a_object, json_file)
 
-        organism = a_object['GBSet']['GBSeq']['GBSeq_organism']
+        # create fasta dictionary
+        try:
+            organism = a_object['GBSet']['GBSeq']['GBSeq_organism']
+            description = f'{accession_id} {organism}'
 
-        description = f'{accession_id} {organism}'
-        if add_location:
-            qualifiers = a_object['GBSet']['GBSeq']['GBSeq_feature-table']['GBFeature'][0]['GBFeature_quals']['GBQualifier']
-            for qualifier in qualifiers:
-                if 'country' in qualifier.get('GBQualifier_name'):
-                    location = qualifier.get('GBQualifier_value')
-                    description += f' {location}'
-                    break
-        fasta = {'description': description, 'sequence': accession['sequence']}
+            if add_location:
+                qualifiers = a_object[
+                    'GBSet']['GBSeq']['GBSeq_feature-table']['GBFeature'][0]['GBFeature_quals']['GBQualifier']
+                for qualifier in qualifiers:
+                    if 'country' in qualifier.get('GBQualifier_name'):
+                        location = qualifier.get('GBQualifier_value')
+                        description += f' {location}'
+                        break
 
-        fastas.append(fasta)
+            sequence = accession['sequence']
+
+            # remove gaps, conditionally
+            sequence = clean_fasta(sequence, remove_gaps=remove_gaps).get('sequence')
+
+            # add fasta to results
+            fasta = {'description': description, 'sequence': sequence}
+            fastas.append(fasta)
+
+        except KeyError as e:
+            logging.error(f'{e}')
+            continue
+
+        # clean up files
+        for file in [xml_file, json_file]:
+            if os.path.exists(file):
+                os.remove(file)
+            else:
+                logging.warn(f'file does not exist: {file}')
 
     return fastas
 
 
 def query(body: dict = None, **kwargs):
     """
-    :param body:
+    Query Genbank for matching sequences
+    :param body: query options
       Example:
       {
         "location": true,
@@ -144,38 +240,63 @@ def query(body: dict = None, **kwargs):
         "sequence": "ACTAtGttGCCTtGGCAGGCTGGCAGCAGCCTGCCGGTGGACCTCAACTCTTGAATCTCTG..."
       }
     :param kwargs:
-    :return:
-      Example: http://jsonblob.com/930240229424775168
+    :return: list of fasta sequence dicts
+      Example:
+      [
+        {
+          "description": "MK373018.1 Cudonia confusa USA",
+          "sequence": "ACTATGTTGCCTTGGCAGGCTGGCAGCAGCCTGCCGGTGGACCTCAACTCTTGAATCTCT..."
+        }
+      ]
     """
+    logging.debug(f'kwargs: {kwargs}')
 
+    # generate Id from body
     id = get_id(body)
-    query_sequence = body['sequence']
+
+    # location enrichment
+    add_location = body.get('location', True)
+
+    # alignment gap removal
+    remove_gaps = body.get('clean', True)
 
     # maximum results
-    results = 20
-    if 'results' in body.keys():
-        results = body['results']
+    max_results = body.get('results', 50)
 
     # minimum identity match
-    match = 95
-    if 'match' in body.keys():
-        match = body['match']
+    min_match = body.get('match', 90)
 
-    # minimum identity match
-    location = True
-    if 'location' in body.keys():
-        location = body['location']
+    # clean fasta query
+    query_fasta = clean_fasta(body.get('sequence'), remove_gaps=remove_gaps)
+    query_description = query_fasta.get('description')
+    query_sequence = query_fasta.get('sequence')
+    logging.debug(f'"query_description": "{query_description}", "query_sequence": {query_sequence}')
 
     # write fas query
     q_file = f'/blast/queries/{id}.fas'
     write_file(q_file, [query_sequence])
 
     # set blastn options
+    word_size = 28
+    output_format = 15
+    database = 'nt'
     o_file = f'/blast/fasta/{id}.json'
-    options = f'-remote -db nt -word_size 28 -outfmt 15 -perc_identity {match} -max_target_seqs {results}'
+    option_list = [
+        '-remote',
+        f'-db {database}',
+        f'-word_size {word_size}',
+        f'-outfmt {output_format}',
+        f'-perc_identity {min_match}',
+        f'-max_target_seqs {max_results}',
+        f'-query {q_file}',
+        f'-out {o_file}'
+    ]
+    options = ' '.join(option_list)
 
-    # run blastn command
-    blastn_command = f'/usr/local/bin/blastn {options} -query {q_file} -out {o_file}'
+    # execute blast query
+    blastn_command = f'/usr/local/bin/blastn {options}'
+    logging.debug(blastn_command)
+
     command_results = execute([blastn_command])
     log_results(command_results)
 
@@ -184,12 +305,25 @@ def query(body: dict = None, **kwargs):
     accessions = load_accessions(blast_results)
 
     # load fasta from accessions
-    resp = load_fastas(id, accessions, add_location=location)
+    resp = load_fasta(id, accessions, add_location=add_location, remove_gaps=remove_gaps)
+
+    # insert query fasta as first record
+    resp.insert(0, {'description': query_description, 'sequence': query_sequence})
+
+    # clean up files
+    for file in [q_file]:
+        if os.path.exists(file):
+            os.remove(file)
+        else:
+            logging.warn(f'file does not exist: {file}')
 
     return resp, 200
 
 
+# todo: add _local_ genbank API sequence methods
+# sequence API method stubs
 def post(body: dict = None, **kwargs):
+    logging.debug(f'kwargs: {kwargs}')
     id = get_id(body)
     resp = {
         'id': id
@@ -198,12 +332,28 @@ def post(body: dict = None, **kwargs):
 
 
 def put(id: str = None, body: dict = None, **kwargs):
+    params = {
+        'id': id,
+        'body': body,
+        'kwargs': kwargs
+    }
+    logging.debug(f'{params}')
     pass
 
 
 def get(id: str = None, **kwargs):
+    params = {
+        'id': id,
+        'kwargs': kwargs
+    }
+    logging.debug(f'{params}')
     pass
 
 
 def delete(id: str = None, **kwargs):
+    params = {
+        'id': id,
+        'kwargs': kwargs
+    }
+    logging.debug(f'{params}')
     pass
